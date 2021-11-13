fedc8ee022eee0fc9a08e77774acdf5d50b8a9ad
AleSilva04 <81649035+AleSilva04@users.noreply.github.com>
2021-11-13 16:15:29 -0500

    close #23

--- Generar_Grafo.py
+++ Generar_Grafo.py
@@ -1,14 +1,17 @@
-import numpy as np
-def crear_grafo(datos):
-  n = len(datos)
-  G = [[None]*n]*n
-  G = np.array(G)
+def add(G, u, n, i, j):
+  if i < 0 or i >= len(G) or j < 0 or j >= len(G):
+    return
+  v = i * n + j
+  G[u].append(v)
 
-  for f in range(n):
-    for c in range(f+1,n):
-      x1,y1 = datos[c][0],datos[c][1]
-      x2,y2 = datos[f][0],datos[f][1]
-      d = ( ((x1-x2)**2) + ((y1-y2)**2) )**(0.5)
-      G[f,c] = d
-      G[c,f] = d
+def generateGridGraph(n):
+  G = [[] for _ in range(n**2)]
+  for i in range(n):
+    for j in range(n):
+      u = i * n + j
+      add(G, u, n, i, j - 1)
+      add(G, u, n, i, j + 1)
+      add(G, u, n, i - 1, j)
+      add(G, u, n, i + 1, j)
+
   return G
